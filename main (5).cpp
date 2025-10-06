cd9b8956dab012a9eca3d2741bac79cd4a17bab3
GABRIELJOKER750 <gzambrano@alumno.montepiedra.edu.ec>
2025-10-06 09:13:07 -0500

    Add files via upload

---
+++ main (5).cpp
@@ -0,0 +1,29 @@
+
+#include <cmath> // Para M_PI
+
+int main() {
+    double radio;
+    std::cout << "Ingrese el radio del círculo: ";
+    std::cin >> radio;
+
+    double area = M_PI * radio * radio;
+    std::cout << "El área del círculo es: " << area << std::endl;
+
+    return 0;
+}
+****************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello World";
+
+    return 0;
+}
